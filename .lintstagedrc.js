6a627f4f10124cbe9cf00999c0ea8d58bbc60a24
Jeroen de Bruijn <vidavidorra+jdbruijn@gmail.com>
2020-04-18 12:14:09 +0200

    chore: add build to TS/JS lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   '*.{ts,tsx,js,jsx,json}': ['npm run lint-es:file:fix'],
+  'src/**/*.{ts,tsx,js,jsx,json}': ['npm run build'],
   '*.{vue,ts,css,less,scss,html,htm,md,markdown,yml,yaml}': [
     'npm run format:file',
   ],
